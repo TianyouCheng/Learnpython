19cf595a4155622a65257a98f3e76daa237bab5e
TianyouCheng <3057580268@qq.com>
2021-09-11 16:43:22 +0800

    新增：条件与循环

--- foundation.py
+++ foundation.py
@@ -1,12 +1,21 @@
-print('hello world 你好世界')
-#字典，通过键来存取，而不是通过偏移存取
-dict={}
-dict['one']="This is one"
-dict[2]="This is two"
+# print('hello world 你好世界')
+# #字典，通过键来存取，而不是通过偏移存取
+# dict={}
+# dict['one']="This is one"
+# dict[2]="This is two"
 
-tinydict={'name':'runoob','code':3745,'dept':'sales'}
+# tinydict={'name':'runoob','code':3745,'dept':'sales'}
 
-print(dict['one'])
-print(tinydict)
-print(tinydict.keys())
-print(tinydict.values())
+# print(dict['one'])
+# print(tinydict)
+# print(tinydict.keys())
+# print(tinydict.values())
+
+# 条件与循环
+a = 1
+while a < 7:
+    if(a % 2 == 0):
+        print(a, "is even")
+    else:
+        print(a, "is odd")
+    a += 1
