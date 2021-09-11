9221daaadd0729ec7f1a59d1d71ed1f7eac538fe
TianyouCheng <3057580268@qq.com>
2021-09-11 19:44:38 +0800

    学习：类与方法

--- learnFunction.py
+++ learnFunction.py
@@ -1,7 +1,10 @@
-import learnmodule
-pr=learnmodule.test(2)
-print(pr)
+# import learnmodule
+# pr=learnmodule.test(2)
+# print(pr)
 
-# from后面只能跟模块名，不能是包名；import后是模块里面的函数或变量等
-from package_runoob.runoob1 import*
-oob1()
+# # from后面只能跟模块名，不能是包名；import后是模块里面的函数或变量等
+# from package_runoob.runoob1 import*
+# oob1()
+from package_runoob.learnClass import*
+lihua=Employee('lihua',50)
+print(lihua.displayCount())
