a7943f5aea008a7fb61a2c0e250b8899a1a767ff
TianyouCheng <3057580268@qq.com>
2021-09-11 17:18:55 +0800

    操作：import math

--- foundation.py
+++ foundation.py
@@ -1,3 +1,4 @@
+import math
 # print('hello world 你好世界')
 # #字典，通过键来存取，而不是通过偏移存取
 # dict={}
@@ -11,13 +12,17 @@
 # print(tinydict.keys())
 # print(tinydict.values())
 
-# 求质数
-i=2
-while(i<100):
-    j=2
-    while(j<=(i/j)):
-        if not(i%j):break
-        j+=1
-    if(j>i/j):print(str(i)+"是质数")
-    i+=1
-print('goodbye')
+# # 求质数
+# i=2
+# while(i<100):
+#     j=2
+#     while(j<=(i/j)):
+#         if not(i%j):break
+#         j+=1
+#     if(j>i/j):print(str(i)+"是质数")
+#     i+=1
+# print('goodbye')
+
+
+#字符串格式化
+print("我叫%s今年%d岁！"%('小明',10))
