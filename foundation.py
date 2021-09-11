9eec2ba762f82126b27339cb03f1aaa737dff8f8
TianyouCheng <3057580268@qq.com>
2021-09-11 16:20:27 +0800

    Create foundation.py

---
+++ foundation.py
@@ -0,0 +1,12 @@
+print('hello world 你好世界')
+#字典，通过键来存取，而不是通过偏移存取
+dict={}
+dict['one']="This is one"
+dict[2]="This is two"
+
+tinydict={'name':'runoob','code':3745,'dept':'sales'}
+
+print(dict['one'])
+print(tinydict)
+print(tinydict.keys())
+print(tinydict.values())
